538d70ef790caea276fe833a8674fdb1d7d97e2e
Fedriana Rahman <fedrianarahman21@gmail.com>
2023-07-25 23:51:23 +0700

    text

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ if (!isset($_SESSION['nama'])) {
                 <div class="row">
                     <div class="col-md-6">
                         <h1> Nikmati Tinggal di Kost dengan Fasilitas Terbaik</h1>
-                        <p>Kosan bu ida adalah kosan yang berada di kota bandung tepatnya di jalan sekeloa tengah. Kosan yang tidak jadu dari monument perjuangan rakyat  sekitar 530 meter dari kosan, sekitar 1,5 km ke gedung sate, berjarak 90 meter dari indomart sekeloa dan  banyak jajanan bandung di sekitaran kosan.</p>
+                        <p>Kosan bu ida adalah kosan yang berada di kota bandung tepatnya di jalan sekeloa tengah. Kosan yang tidak jauh dari monument perjuangan rakyat  sekitar 530 meter dari kosan, 1,5 km ke gedung sate, berjarak 90 meter dari indomart sekeloa dan  banyak jajanan bandung di sekitaran kosan.</p>
                         <a href="./room.php" class="btn  btn-custom">Show Me Now</a>
                         <!-- <div class="hero-icon">
                             <div class="hero-icon-item">
